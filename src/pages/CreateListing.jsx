7c0f107c3dba5a81957c47808f6375f71e520ee0
Jose Lara-Urbina <joselaraurb@gmail.com>
2024-03-07 01:09:08 -0500

    Fixed issue with address not saving correctly

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -119,7 +119,6 @@ function CreateListing() {
       // geolocation.lat = latitude;
       // geolocation.lng = longitude;
       geopoint = new GeoPoint(latitude, longitude);
-      location = address;
     }
 
     // Store image in firebase
@@ -177,9 +176,9 @@ function CreateListing() {
       timestamp: serverTimestamp(),
     };
 
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
